16b216313b3eb1a2678fb93ec1901f4526eddf0c
Giovani Aguirre <me@its-g.io>
2020-03-31 09:45:11 -0700

    Auth template complete.

---
+++ server/controllers/auth.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcryptjs');
+
+async function register(req, res) {
+  const { email, password } = req.body;
+  const db = req.app.get('db');
+
+  const foundUser = await db.user.get_user(email).catch(err => console.error(err));
+  if (foundUser.length !== 0) return res.status(406).json('email Taken');
+
+  const hash = bcrypt.hashSync(password, 12);
+  const registeredUser = await db.user.register(email, hash);
+
+  const user = registeredUser[0];
+  req.session.user = { email: user.email, user_id: user.user_id };
+
+  return res.status(201).json(req.session.user);
+}
+
+async function login(req, res) {
+  const { email, password } = req.body;
+  const db = req.app.get('db');
+
+  const foundUser = await db.user.get_user(email).catch(err => console.error(err));
+  if (foundUser.length === 0) return res.status(401).json('User Not Found');
+  const user = foundUser[0];
+  const isAuth = await bcrypt.compare(password, user.password);
+  if (isAuth) {
+    req.session.user = { email: user.email, user_id: user.user_id };
+    return res.status(202).json(req.session.user);
+  }
+
+  return res.status(401).json('email or Password incorrect');
+}
+
+function logout(req, res) {
+  req.session.destroy();
+  res.sendStatus(200);
+}
+
+function getSession(req, res) {
+  return req.session.user ? res.status(200).json(req.session.user) : res.status(403).json('Session does not exist');
+}
+
+module.exports = {
+  register,
+  login,
+  logout,
+  getSession
+}
---
+++ server/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const massive = require("massive");
+const session = require("express-session");
+
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+const { register, login, logout, getSession } = require('./controller/auth');
+
+massive(CONNECTION_STRING)
+  .then(db => app.set('db', db))
+  .catch(err => console.error(err));
+
+app
+  .use(express.json())
+  .use(
+    session({
+      secret: SESSION_SECRET,
+      saveUninitialized: true,
+      resave: false,
+    })
+  )
+  .post('/auth/register', register)
+  .post('/auth/login', login)
+  .get('/auth/logout', logout)
+  .get('/auth/session', getSession)
+
+app.listen(SERVER_PORT, () => console.log(`Roger Rodger on port ${SERVER_PORT}`));
