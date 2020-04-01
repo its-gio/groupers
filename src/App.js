357885412fa9138d4db21ed3f5cc85396070b23d
Giovani Aguirre <me@its-g.io>
2020-04-01 15:04:54 -0700

    Added Navigation component to app with light styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import routes from './routes';
-import './App.scss';
 import { HashRouter } from 'react-router-dom';
+import routes from './routes';
+import Nav from './Components/Nav/Nav'
+import './App.scss';
 
 function App() {
   return (
     <HashRouter>
       <div>
+        <Nav />
         { routes }
       </div>
     </HashRouter>
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+      <div><h1>Groupers</h1></div>
+      <ul className="entrance">
+        <li>Login</li>
+        <li>Register</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
