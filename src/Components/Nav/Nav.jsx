9796b55e7faeb27c1480470a495388ea1a18c12f
Giovani Aguirre <me@its-g.io>
2020-04-02 09:44:28 -0700

    When logged in, components now switch fron Entrance to Profile.

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Entrance from './Entrance';
+import Profile from './Profile';
 import { connect } from 'react-redux';
 
-function Nav() {
+function Nav(props) {
   return (
     <nav>
       <div><h1>Groupers</h1></div>
-      <Entrance />
+      { props.user.user_id ? <Profile user={props.user} /> : <Entrance /> }
     </nav>
   )
 }
