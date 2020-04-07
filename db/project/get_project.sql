b18d50b9c1179537f73b59e04436c7f3f23d230a
Giovani Aguirre <me@its-g.io>
2020-04-07 13:36:28 -0700

    Single project gets proper info now.

--- db/project/get_project.sql
+++ db/project/get_project.sql
@@ -1,3 +1,5 @@
-SELECT *
-FROM groupers_project
+SELECT p.project_id, u.fullname, u.profile_pic, p.title, p.description, p.difficulty, p.funded, p.start_time, p.end_time
+FROM groupers_project p
+  LEFT JOIN groupers_users u
+  ON p.creator = u.user_id
 WHERE project_id = $1
--- src/Components/Project/ProjectDetails.jsx
+++ src/Components/Project/ProjectDetails.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 function ProjectDetails(props) {
-  const { title, creator, difficulty, description } = props.project
+  const { title, fullname, difficulty, description } = props.project
   return (
     <>
       <h2>{title}</h2>
-      <h6>by: {creator ? creator : "Anon"}</h6>
+      <h6>by: {fullname ? fullname : "Anon"}</h6>
       <div className="project--DD">
         <span>Description:</span>
         <span>Difficulty: {difficulty}</span>
