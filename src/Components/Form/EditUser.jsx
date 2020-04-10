d2d5c398fc462ed8495d4cb8121bb8f9d308783f
Giovani Aguirre <me@its-g.io>
2020-04-09 20:35:58 -0700

    Values are now added to state on componentDidMount

--- src/Components/Form/EditUser.jsx
+++ src/Components/Form/EditUser.jsx
@@ -2,19 +2,28 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class EditUser extends Component {
+  state = {
+    fullname: '',
+    email: ''
+  }
 
-  
+  componentDidMount() {
+    const { fullname, email } = this.props.user;
+    this.setState({ fullname, email });
+  }
+
   render() {
     return (
       <form className="profile--edit-user__form">
         <span onClick={() => this.props.setEditForm(false)} className="exit">X</span>
-        <input type="text" placeholder="Full Name" />
-        <input type="text" placeholder="Email" />
+        <input type="text" value={this.state.fullname} placeholder="Full Name" />
+        <input type="text" value={this.state.email} placeholder="Email" />
         <button type="submit">Submit</button>
       </form>
     )
   }
 }
 
+const mapStateToProps = reduxState => ({ user: reduxState.user })
 
-export default connect(null, { })(EditUser);
+export default connect(mapStateToProps, { })(EditUser);
