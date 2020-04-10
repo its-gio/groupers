87337724dfc7149135effa3ea8887c3899c78ea6
Giovani Aguirre <me@its-g.io>
2020-04-09 21:29:14 -0700

    Added section for amount and removed it if no amount exists.

--- src/Components/Project/ProjectDetails.jsx
+++ src/Components/Project/ProjectDetails.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 function ProjectDetails(props) {
-  const { title, fullname, difficulty, description } = props.project
+  const { title, fullname, difficulty, description, amount } = props.project
   return (
     <>
-      <h2>{title}</h2>
-      <h6>by: {fullname ? fullname : "Anon"}</h6>
+      <div className="project--header">
+        <div className="project--header__title">
+          <h2>{title}</h2>
+          <h6>by: {fullname ? fullname : "Anon"}</h6>
+        </div>
+
+        { amount ? <h4>Amount: ${amount}</h4> : ''}
+      </div>
       <div className="project--DD">
         <span>Description:</span>
         <span>Difficulty: {difficulty}</span>
